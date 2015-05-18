ef9cb7e5dbfc1eb624a1f38db217fbd2346e73b9
milezoom <milezoom@muslims-notebook>
2015-05-18 08:46:14 +0700

    update link

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -36,16 +36,14 @@ AppAsset::register($this);
                 'options' => ['class' => 'navbar-nav navbar-right'],
                 'items' => [
                     ['label' => 'Home', 'url' => ['/site/index']],
-                    ['label' => 'About', 'url' => ['/site/about']],
-                    ['label' => 'Contact', 'url' => ['/site/contact']],
                     ['label' => 'Keuntungan', 'url' => ['/site/keuntungan']],
                     ['label' => 'Awareness', 'url' => ['/site/awareness']],
                     ['label' => 'Upload', 'url' => ['/site/upload']],
-                    Yii::$app->user->isGuest ?
+                    /*Yii::$app->user->isGuest ?
                         ['label' => 'Login', 'url' => ['/site/login']] :
                         ['label' => 'Logout (' . Yii::$app->user->identity->username . ')',
                             'url' => ['/site/logout'],
-                            'linkOptions' => ['data-method' => 'post']],                    
+                            'linkOptions' => ['data-method' => 'post']],*/
                 ],
             ]);
             NavBar::end();
